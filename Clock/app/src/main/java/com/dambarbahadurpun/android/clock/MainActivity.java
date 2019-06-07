56f9e0e946d2827a1f55e5cefdff378522f83456
Dambar Pun <>
2019-06-07 13:21:00 +0545

    icons and menu for tab

--- Clock/app/src/main/java/com/dambarbahadurpun/android/clock/MainActivity.java
+++ Clock/app/src/main/java/com/dambarbahadurpun/android/clock/MainActivity.java
@@ -1,9 +1,24 @@
 package com.dambarbahadurpun.android.clock;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private TabLayout mTabLayout;
+    private TabItem mTabItemWorldClock;
+    private TabItem mTabItemAlarm;
+    private TabItem mTabItemTimer;
+    private TabItem mTabItemStopWatch;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        mViewPager = findViewById(R.id.view_pager);
+        mToolbar = findViewById(R.id.toolbar);
+        mTabItemAlarm = findViewById(R.id.tab_item_alarm);
+        mTabItemStopWatch = findViewById(R.id.tab_item_stop_watch);
+        mTabItemTimer = findViewById(R.id.tab_item_timer);
+        mTabItemWorldClock = findViewById(R.id.action_add_world_clock);
+    }
 
+    public void addWorldClock(MenuItem item) {
+        Toast.makeText(this, "Menu Clicked", Toast.LENGTH_SHORT).show();
     }
 }
